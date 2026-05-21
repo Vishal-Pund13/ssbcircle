a6a2c0813dca4ab11e3ec4a5c63bc964683cd25a
unknown <vishal3pund@gmail.com>
2026-05-21 22:25:36 +0530

    Remove GA debug logs

--- frontend/src/analytics.js
+++ frontend/src/analytics.js
@@ -1,7 +1,6 @@
 import ReactGA from 'react-ga4';
 
 export const initGA = () => {
-  console.log('GA ID:', import.meta.env.VITE_GA_ID);
   if (import.meta.env.VITE_GA_ID) {
     ReactGA.initialize(import.meta.env.VITE_GA_ID);
   }
